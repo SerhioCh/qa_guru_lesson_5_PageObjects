981c0ec77443cf4f5756f279020450ca3ca4d3f2
Сергей Квин <lorancem@mail.ru>
2022-10-12 01:01:43 +0300

    home work lesson 6

--- src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
@@ -1,46 +1,42 @@
 package com.demoqa.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.demoqa.pages.RegistrationFormPage;
-import org.junit.jupiter.api.BeforeAll;
+import com.demoqa.utils.RandomUtils;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationFormWithPageObjectsTests {
+public class RegistrationFormWithPageObjectsTests extends RandomUtils {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
-    @BeforeAll
-    static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
-
-    }
     @Test
     void fillFormTest() {
         registrationFormPage.openPage()
-                .setFirstName("Serega")
-                .setLastName("Romanovich")
-                .setEmail("Serega@romanovich.com")
-                .setGender("Other")
-                .setNumber("1234567890")
-                .setBirthDate("30", "July", "2008")
-                .setSubjects("Math")
-                .setHobbiesInTheCheckbox("Sports");
-            registrationFormPage.uploadFile("img1.png")
-                .setCurrentAddress("Some address 1")
-                .setStateOnTheSelect("NCR")
-                .setCityOnTheSelect("Delhi")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setNumber(phoneNumber)
+                .setBirthDate(String.valueOf(BirthdayDay), String.valueOf(BirthdayMonth), BirthdayYear)               //.setBirthDate("30", "July", "2008")
+                .setSubjects(subjects)
+                .setHobbiesInTheCheckbox(hobbies);
+        registrationFormPage.uploadFile("img1.png")
+                .setCurrentAddress(currentAddress)
+                .setStateOnTheSelect(state)
+                .setCityOnTheSelect(city)
                 .clickOnButtonSubmit();
             registrationFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", "Serega"+" "+"Romanovich")
-                .checkResult("Student Email", "Serega@romanovich.com")
-                .checkResult("Gender", "Other")
-                .checkResult("Mobile", "1234567890" )
-                .checkResult("Date of Birth", "30"+" "+"July,2008")
-                .checkResult("Subjects", "Maths")
-                .checkResult("Hobbies", "Sports")
+                .checkResult("Student Name", firstName+" "+lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phoneNumber )
+                .checkResult("Date of Birth", BirthdayDay+" "+BirthdayMonth+","+BirthdayYear)
+                .checkResult("Subjects", subjects)
+                .checkResult("Hobbies", hobbies)
                 .checkResult("Picture", "img1.png")
-                .checkResult("Address", "Some"+" "+"address"+" "+"1")
-                .checkResult("State and City", "NCR"+" "+"Delhi");
+                .checkResult("Address", currentAddress)
+                .checkResult("State and City", state+" "+city);
     }
+
+
+
+
 }
---
+++ src/test/java/com/demoqa/utils/RandomUtils.java
@@ -0,0 +1,52 @@
+package com.demoqa.utils;
+
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationFormPage;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+public class RandomUtils {
+    Faker faker = new Faker(new Locale("eng"));
+   public String firstName = faker.name().firstName(),
+     lastName = faker.name().lastName(),
+    email = faker.internet().emailAddress(),
+     gender = "Other",
+     phoneNumber = faker.phoneNumber().subscriberNumber(10),
+     BirthdayDay = String.valueOf(faker.number().numberBetween(10, 27)),
+     BirthdayMonth = getRandomMonth(),
+     BirthdayYear = String.valueOf(faker.number().numberBetween(1999, 2005)),
+     subjects = "Math",
+     hobbies = "Sports",
+     currentAddress = faker.address().fullAddress(),
+     state = "NCR",
+     city = "Delhi";
+    public static String getRandomMonth() {
+        List<String> months = Arrays.asList(
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December");
+
+        int index = new Random().nextInt(months.size());
+        return months.get(index);
+    }
+
+
+
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        //Configuration.pageLoadTimeout=150000;
+        Configuration.browserSize = "1920x1080";
+    }
+}
+
+
